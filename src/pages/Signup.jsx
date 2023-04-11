d36ca2be764451eb869704e67785f5d5f214db1b
jeonhyun <jeonhyun@student.42seoul.kr>
2023-04-11 15:08:15 +0900

    [feat] data-test-id 추가

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,9 +3,9 @@ function Signup() {
     <div>
       <h1>회원가입</h1>
       <form>
-        <input value={'이메일'} />
-        <input value={'패스워드'} />
-        <button>제출</button>
+        <input data-testid='email-input' value={'이메일'} />
+        <input data-testid='password-input' value={'패스워드'} />
+        <button data-testid='signup-button'>회원가입</button>
       </form>
     </div>
   )
