68b1cb00a344b67e097e18515fff589b4141485b
jeonhyun <jeonhyun@student.42seoul.kr>
2023-04-11 14:43:15 +0900

    [feat] 로그인 컴포넌트 추가

---
+++ src/pages/Signin.js
@@ -0,0 +1,14 @@
+function Signin() {
+  return (
+    <div>
+      <h1>로그인</h1>
+      <form>
+        <input>이메일</input>
+        <input>패스워드</input>
+        <button>제출</button>
+      </form>
+    </div>
+  )
+}
+
+export default Signin
