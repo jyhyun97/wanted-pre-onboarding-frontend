73d54af1bb3ae0a71a5ecea5fa90f76b18333f93
jeonhyun <jeonhyun@student.42seoul.kr>
2023-04-11 17:13:34 +0900

    [feat] 버튼 클릭 시 회원가입 추가

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,24 @@
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 function Signup() {
   const [emailValue, setEmailValue] = useState('')
   const [passwordValue, setPasswordValue] = useState('')
   const [valueTerm, setValueTerm] = useState(false)
+
+  const submitData = () => {
+    axios
+      .post('https://www.pre-onboarding-selection-task.shop/auth/signup', {
+        email: emailValue,
+        password: passwordValue,
+      })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
   useEffect(() => {
     setValueTerm(emailValue.includes('@') && passwordValue.length >= 8)
@@ -25,6 +40,7 @@ function Signup() {
           data-testid='signup-button'
           type={'button'}
           disabled={!valueTerm}
+          onClick={submitData}
         >
           회원가입
         </button>
