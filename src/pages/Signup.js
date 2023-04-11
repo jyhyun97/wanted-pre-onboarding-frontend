2ae453c311c1d6250a6752021e3e5672544846aa
jeonhyun <jeonhyun@student.42seoul.kr>
2023-04-11 14:46:14 +0900

    [feat] 회원가입 페이지 추가

---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+function Signup() {
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <form>
+        <input>이메일</input>
+        <input>패스워드</input>
+        <button>제출</button>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
