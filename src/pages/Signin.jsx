8e5dac31a914ef6ba6ffffb955d61b5a897158c7
jeonhyun <jeonhyun@student.42seoul.kr>
2023-04-12 15:20:41 +0900

    [feat] test-id 추가 및 기본 기능 구현

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,11 +1,49 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 function Signin() {
+  const [emailValue, setEmailValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
+  const [valueTerm, setValueTerm] = useState(false)
+
+  const submitData = () => {
+    axios
+      .post('https://www.pre-onboarding-selection-task.shop/auth/signin', {
+        email: emailValue,
+        password: passwordValue,
+      })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    setValueTerm(emailValue.includes('@') && passwordValue.length >= 8)
+  }, [emailValue, passwordValue])
+
   return (
     <div>
       <h1>로그인</h1>
       <form>
-        <input value={'이메일'} />
-        <input value={'패스워드'} />
-        <button>제출</button>
+        <input
+          data-testid='email-input'
+          onChange={(e) => setEmailValue(e.target.value)}
+        />
+        <input
+          data-testid='password-input'
+          onChange={(e) => setPasswordValue(e.target.value)}
+        />
+        <button
+          data-testid='signin-button'
+          type={'button'}
+          disabled={!valueTerm}
+          onClick={submitData}
+        >
+          로그인
+        </button>
       </form>
     </div>
   )
